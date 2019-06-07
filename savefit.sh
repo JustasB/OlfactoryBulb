102103d2b3e6c70f7c28a48ac16c5a6ff176bb92
Justas Birgiolas <JustasB@users.noreply.github.com>
2019-06-06 20:30:28 -0700

    Update savefit.sh

--- savefit.sh
+++ savefit.sh
@@ -2,7 +2,7 @@ branch_name=fit-`date +%Y%m%d%H%M%S`
 git branch $branch_name
 git checkout $branch_name
 
-python fit.py &> fit.txt
+python fit.py | tee /dev/tty > fit.txt
 
 git add .
 git commit -m 'fit results'
